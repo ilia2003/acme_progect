5504e385455c077541c47cef4be2d419ce947488
Fedorenko Ilia <fedorenkoilia@gmail.com>
2023-12-24 13:10:40 +0700

    1

---
+++ acme_project/birthday/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.urls import reverse
+# Импортируется функция-валидатор.
+from .validators import real_age
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', max_length=20, help_text='Необязательное поле', blank=True
+    )
+    # Валидатор указывается в описании поля.
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+    author = models.ForeignKey(
+        User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
+
+    class Meta:
+
+        constraints = (
+             models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+                ),
+            )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
+
+
+class Congratulation(models.Model):
+    text = models.TextField('Текст поздравления')
+    birthday = models.ForeignKey(
+        Birthday,
+        on_delete=models.CASCADE,
+        related_name='congratulations',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
---
+++ acme_project/birthday/urls.py
@@ -0,0 +1,18 @@
+# birthday/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'birthday'
+
+urlpatterns = [
+    path('', views.BirthdayCreateView.as_view(), name='create'),
+    path('list/', views.BirthdayListView.as_view(), name='list'),
+    path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
+    path('create/', views.BirthdayCreateView.as_view(), name='create'),
+    path('update/', views.BirthdayUpdateView.as_view(), name='update'),
+    path('delete/', views.BirthdayDeleteView.as_view(), name='delete'),
+    path('<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete')
+]
